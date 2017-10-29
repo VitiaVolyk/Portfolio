ca592c4ddfa9f1d655fc2e96341f5f2567113401
VitiaVolyk <Vitia.Volyk@gmail.com>
2017-10-29 22:26:40 +0200

    third com

--- js/common.js
+++ js/common.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
+	$(".mc_item_wrap ul").each(function() {
+		$(this).after("<div class='mc_item_wrap_af'></div>");
+	});
+	$(".mc_toggle").click(function() {
+		if ($(this).parent().parent().children("ul").is(":visible")) {
+			$(this).parent().parent().children("ul").slideUp();
+			$(this).parent().parent().children(".mc_item_wrap_af").hide();
+		} else {
+			$("body .mc_wrap .mc_item_wrap > ul, .mc_item_wrap_af").hide();
+			$(".mc_item_wrap").removeClass("active");
+			$(this).parent().parent().addClass("active");
+			$(this).parent().parent().children("ul").slideDown();
+			$(this).parent().parent().children(".mc_item_wrap_af").show();
+			}
+	});
+	
+	
 
 	//Цели для Яндекс.Метрики и Google Analytics
 	$(".count_element").on("click", (function() {
